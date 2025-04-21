ca8d7860fe422f85a6eeeb19cac3e4b3dd446f80
ranandasatria <rananda24@gmail.com>
2025-04-21 23:21:02 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -1,32 +1,51 @@
 // 1. Deteksi palindrom 
+
 // a. Menggunakan built-in method
 
-// const inputText = "LevEl";
-// console.log(inputText.toLowerCase() === inputText.toLowerCase().split("").reverse().join("") ? "Palindrom" : "Bukan palindrom");
+const inputText = "LevEl";
+console.log(inputText.toLowerCase() === inputText.toLowerCase().split("").reverse().join("") ? "Palindrom" : "Bukan palindrom");
 
 // b. Secara manual
 
-function palindromeDetection (word){
-    //belum ubah huruf besar kecil
-    if(typeof word !== "string")
+function palindromeDetection (inputText1){
+    let lowerText1 = inputText1.toLowerCase() 
+    if(typeof lowerText1 !== "string")
         return console.log("Hanya bisa memasukkan huruf")   
     let reversed = ""
-    for (i = 0; i < word.length; i++){
-        reversed += word[word.length-1-i]
+    for (i = 0; i < lowerText1.length; i++){
+        reversed += lowerText1[lowerText1.length-1-i]
     }
-    
-    if (word === reversed) 
+    if (lowerText1 === reversed) 
         console.log("Palindrom")
     else 
         console.log("Bukan Palindrom")
 }
-
-palindromeDetection("level")
-
+palindromeDetection("LevEl")
 
 
-// 2. Deteksi palindrom pakai built-in method
 
-// const kalimat = "saya suka belajar JavaScript";
-// console.log(kalimat.split(" ").reverse().join(" ")); 
+// 2. Deteksi palindrom 
 
+// a. Menggunakan built-in method
+
+const kalimat0 = "Saya suka belajar JavaScript";
+console.log(kalimat0.split(" ").reverse().join(" ")); 
+
+// b. Secara manual
+
+function reverseWords(text) {
+    let kalimatTerbalik = ""; 
+    let kataSementara = ""; 
+    for (let i = text.length - 1; i >= 0; i--) {
+        if (text[i] === " ") {
+            kalimatTerbalik += kataSementara + " ";
+            kataSementara = "";
+        } else {
+            kataSementara = text[i] + kataSementara;
+        }
+    }
+    kalimatTerbalik += kataSementara;
+    return kalimatTerbalik
+}
+const kalimat = "saya suka belajar JavaScript";
+console.log(reverseWords(kalimat))
