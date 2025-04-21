5bc702e88393506222c983d454c865025fd50593
ranandasatria <rananda24@gmail.com>
2025-04-21 15:32:47 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -1,9 +1,32 @@
-//1. Deteksi palindrom pakai built-in method
+// 1. Deteksi palindrom 
+// a. Menggunakan built-in method
 
-const inputText = "LevEl";
-console.log(inputText.toLowerCase() === inputText.toLowerCase().split("").reverse().join("") ? "Palindrom" : "Bukan palindrom");
+// const inputText = "LevEl";
+// console.log(inputText.toLowerCase() === inputText.toLowerCase().split("").reverse().join("") ? "Palindrom" : "Bukan palindrom");
 
-//2. Deteksi palindrom pakai built-in method
+// b. Secara manual
 
-const kalimat = "saya suka belajar JavaScript";
-console.log(kalimat.split(" ").reverse().join(" ")); 
+function palindromeDetection (word){
+    //belum ubah huruf besar kecil
+    if(typeof word !== "string")
+        return console.log("Hanya bisa memasukkan huruf")   
+    let reversed = ""
+    for (i = 0; i < word.length; i++){
+        reversed += word[word.length-1-i]
+    }
+    
+    if (word === reversed) 
+        console.log("Palindrom")
+    else 
+        console.log("Bukan Palindrom")
+}
+
+palindromeDetection("level")
+
+
+
+// 2. Deteksi palindrom pakai built-in method
+
+// const kalimat = "saya suka belajar JavaScript";
+// console.log(kalimat.split(" ").reverse().join(" ")); 
+
