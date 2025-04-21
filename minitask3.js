4888ab0e0d05091c1ed2b2f8edf4fce2f4bb7b5d
ranandasatria <rananda24@gmail.com>
2025-04-21 14:23:37 +0700

    Add files via upload

--- minitask3.js
+++ minitask3.js
@@ -1,10 +1,5 @@
 const animal = "ayam,bebek,sapi,unta"
 
 console.log(animal.split(",").with(2, "domba").join())
-
-//chaining built-in method
-// let arr = animal.split(",")
-// arr[2] = "domba"
-// const gabung = arr.join(",")
-
-// console.log(gabung)
+console.log(animal.split(",").toSpliced(2, 1, "domba").join())
+console.log(animal.replace("sapi", "domba"))
